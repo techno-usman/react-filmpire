8d3691580eca5200a84d67dd54db46bdb992a8d4
Muhammad Usman Ch <developermolvi@gmail.com>
2022-07-17 15:16:46 +0500

    added styling structure using material UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import useStyles from './styles';
 import {
   Movies, Actors, MovieInformation, Profile, NavBar,
 } from './index';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Movies />}>
-              <Route path=":movieId" element={<MovieInformation />} />
-            </Route>
-            <Route path="actors" element={<Actors />}>
+            <Route path="/" element={<Movies />} />
+            <Route path="movie/:movieId" element={<MovieInformation />} />
+            <Route path="/actors" element={<Actors />}>
               <Route path=":actorId" element={<Actors />} />
             </Route>
             <Route path="/profile/:profileId" element={<Profile />} />
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Movies() {
   return (
-    <div>Movie</div>
+    <div>Movies</div>
   );
 }
 
